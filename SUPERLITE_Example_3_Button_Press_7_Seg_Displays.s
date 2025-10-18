6503cf04cabc85477c8840044eb59b1d7cde787a
acelectronics <antonioastro@proton.me>
2025-10-18 17:27:17 +0100

    Updated 7-segment Display Codes

--- SUPERLITE_Example_3_Button_Press_7_Seg_Displays.s
+++ SUPERLITE_Example_3_Button_Press_7_Seg_Displays.s
@@ -18,7 +18,7 @@ CONFIG  IESO = ON             ; Internal External Switchover bit (Internal Exter
 psect RES_VECT,class=CODE,delta=2 ; PIC10/12/16. The line "-PRES_VECT=0x00" must be present as a custom linker option in the project properties.
 RES_VECT:
     bsf STATUS,5	;change RP0 (bit 5 of STATUS register) to 1 to select BANK1
-    movlw 00000001	
+    movlw 00100000	
     movwf TRISA		;configure PORTA,0 as INPUT, 1-7 OUTPUT
     movlw 00000000
     movwf TRISB		;configure PORTB as OUTPUT
@@ -30,22 +30,22 @@ COUNT_TEMP EQU 0x41 ;define a temporary location to use the value in COUNT for o
 NUM_10s	EQU 0x42 ;define a pointer to the file register for converting the count to BCD
 NUM_1s	EQU 0x43
 
-NINE	EQU 11011110B ;define the digits to display on PORTA or PORTB
-EIGHT	EQU 11111110B
-SEVEN	EQU 00001110B
-SIX	EQU 11111010B
-FIVE	EQU 11011010B
-FOUR	EQU 11001100B
-THREE	EQU 10011110B
-TWO	EQU 10110110B
-ONE	EQU 00001100B
-ZER0	EQU 01111110B
+NINE	EQU 11001111B ;define the digits to display on PORTA or PORTB
+EIGHT	EQU 11011111B
+SEVEN	EQU 00000111B
+SIX	EQU 11011101B
+FIVE	EQU 11001101B
+FOUR	EQU 11000110B
+THREE	EQU 10001111B
+TWO	EQU 10011011B
+ONE	EQU 00000110B
+ZER0	EQU 01011111B
 
 clrf COUNT ;initially set the value in COUNT to zero.
 clrf COUNT_TEMP
 
 main:
-    btfss PORTA,0
+    btfss PORTA,5   ;PORTA5 is the only INPUT on PORTA.
     goto main
     incf COUNT,1    ;increment the value in COUNT
     movf COUNT,0x00 ;copy COUNT to working register
